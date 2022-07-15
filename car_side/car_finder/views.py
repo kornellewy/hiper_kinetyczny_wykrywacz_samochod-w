b2771094e57f79fcf6a845d859dc383367382cb8
kornellewy <40360922+kornellewy@users.noreply.github.com>
2022-07-16 00:38:09 +0200

    fix: small program fix

--- car_side/car_finder/views.py
+++ car_side/car_finder/views.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-# import cv2
+import cv2
 
 from django.shortcuts import render
 from django.views.generic import ListView
@@ -10,7 +10,7 @@ from django.conf import settings
 
 from .forms import FileFieldForm
 from .models import CarImage
-# from .detectron_car_class import DetectronCarClass
+from .detectron_car_class import DetectronCarClass
 
 # Create your views here.
 
@@ -18,7 +18,7 @@ class FileFieldView(FormView):
     form_class = FileFieldForm
     template_name = 'car_finder/home.html'
     success_url = reverse_lazy('result_review')
-    # detector = DetectronCarClass()
+    detector = DetectronCarClass()
 
     def post(self, request, *args, **kwargs):
         form_class = self.get_form_class()
@@ -27,11 +27,11 @@ class FileFieldView(FormView):
         if form.is_valid():
             instance = CarImage(image=file, output_image=file)
             instance.save()
-            # image = cv2.imread(str(instance.image.path))
-            # image, number_of_cars = self.detector.detect(image=image)
-            # cv2.imwrite(str(instance.output_image.path), image)
-            # instance.number_of_cars = number_of_cars
-            # instance.save()
+            image = cv2.imread(str(instance.image.path))
+            image, number_of_cars = self.detector.detect(image=image)
+            cv2.imwrite(str(instance.output_image.path), image)
+            instance.number_of_cars = number_of_cars
+            instance.save()
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
